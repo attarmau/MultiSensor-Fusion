e517fcd55b89ddad7f3be618d538f5163f93af0b
attarmau <attarmau111@gmail.com>
2025-06-05 10:27:08 +0800

    Update calibrate_single.py

--- multi_cam_calib/calibrate_single.py
+++ multi_cam_calib/calibrate_single.py
@@ -5,14 +5,6 @@ import argparse
 import json
 import os
 
-def find_chessboard_corners(image_path, board_size):
-    img = cv2.imread(image_path)
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    ret, corners = cv2.findChessboardCorners(gray, board_size, None)
-    if ret:
-        corners = cv2.cornerSubPix(gray, corners, (11,11), (-1,-1), (cv2.TermCriteria_EPS + cv2.TermCriteria_MAX_ITER, 30, 0.001))
-    return ret, corners, gray.shape[::-1]
-
 def calibrate_camera(image_folder, board_size, square_size):
     objp = np.zeros((board_size[0]*board_size[1], 3), np.float32)
     objp[:, :2] = np.mgrid[0:board_size[0], 0:board_size[1]].T.reshape(-1, 2)
@@ -20,25 +12,42 @@ def calibrate_camera(image_folder, board_size, square_size):
 
     objpoints = []
     imgpoints = []
-    image_size = None
 
     images = sorted(glob.glob(os.path.join(image_folder, '*.jpg')))
 
     for fname in images:
-        ret, corners, img_size = find_chessboard_corners(fname, board_size)
+        img = cv2.imread(fname)
+        if img is None:
+            print(f'[WARN] Could not read {fname}')
+            continue
+
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+        ret, corners = cv2.findChessboardCorners(
+            gray, board_size,
+            cv2.CALIB_CB_ADAPTIVE_THRESH + 
+            cv2.CALIB_CB_FAST_CHECK + 
+            cv2.CALIB_CB_NORMALIZE_IMAGE)
+            
         if ret:
             objpoints.append(objp)
+            corners2 = cv2.cornerSubPix(
+                gray, corner, (11, 11), (-1, -1),
+                (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001))
             imgpoints.append(corners)
-            image_size = img_size
+
+            vis_img = cv2.drawChessboardCorners(img, board_size, corners2, ret)
+            cv2.imshow('Corners', vis_img)
+            cv2.waitKey(500)
         else:
             print(f"[WARN] Chessboard not found in {fname}")
 
     ret, camera_matrix, dist_coeffs, rvecs, tvecs = cv2.calibrateCamera(
-        objpoints, imgpoints, image_size, None, None)
+        objpoints, imgpoints, gray.shape[::-1], None, None)
 
     return camera_matrix, dist_coeffs
 
-def save_intrinsics(filename, mtx, dist):
+def save_intrinsics(filename, camera_martix, dist_coeffs, rvecs, tvecs):
     data = {
         'camera_matrix': mtx.tolist(),
         'dist_coeffs': dist.tolist()
