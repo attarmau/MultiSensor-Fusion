24afbe658135cd9ff1b9105445d1bce0f425f59e
attarmau <attarmau111@gmail.com>
2025-06-05 13:44:24 +0800

    Update calibrate_single.py

--- multi_cam_calib/calibrate_single.py
+++ multi_cam_calib/calibrate_single.py
@@ -1,78 +1,45 @@
-import cv2
-import numpy as np
-import glob
-import argparse
-import json
 import os
+import glob
+import json
+import numpy as np
+import cv2
 
-def calibrate_camera(image_folder, board_size, square_size):
-    objp = np.zeros((board_size[0]*board_size[1], 3), np.float32)
-    objp[:, :2] = np.mgrid[0:board_size[0], 0:board_size[1]].T.reshape(-1, 2)
-    objp *= square_size
+with open('/Users/judyhuang/Downloads/MultiSensor-Fusion-main/data/intrinsics_cam1.json', 'r') as f:
+    data1 = json.load(f)
+camera_matrix1 = np.array(data1['camera_matrix'])
+dist_coeffs1 = np.array(data1['dist_coeffs'])
 
-    objpoints = []
-    imgpoints = []
+with open('/Users/judyhuang/Downloads/MultiSensor-Fusion-main/data/intrinsics_cam2.json', 'r') as f:
+    data2 = json.load(f)
+camera_matrix2 = np.array(data2['camera_matrix'])
+dist_coeffs2 = np.array(data2['dist_coeffs'])
 
-    images = sorted(glob.glob(os.path.join(image_folder, '*.jpg')))
+input_dir_cam1 = '/Users/judyhuang/Downloads/MultiSensor-Fusion-main/data/intrinsic/cam1'
+output_dir_cam1 = '/Users/judyhuang/Downloads/MultiSensor-Fusion-main/data/intrinsic/undistorted_cam1'
+input_dir_cam2 = '/Users/judyhuang/Downloads/MultiSensor-Fusion-main/data/intrinsic/cam2'
+output_dir_cam2 = '/Users/judyhuang/Downloads/MultiSensor-Fusion-main/data/intrinsic/undistorted_cam2'
 
-    for fname in images:
-        img = cv2.imread(fname)
-        if img is None:
-            print(f'[WARN] Could not read {fname}')
-            continue
+os.makedirs(output_dir_cam1, exist_ok=True)
+os.makedirs(output_dir_cam2, exist_ok=True)
 
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+for img_path in glob.glob(os.path.join(input_dir_cam1, '*.jpg')):
+    img = cv2.imread(img_path)
+    if img is not None:
+        undistorted = cv2.undistort(img, camera_matrix1, dist_coeffs1)
+        filename = os.path.basename(img_path)
+        save_path = os.path.join(output_dir_cam1, f'undistorted_{filename}')
+        cv2.imwrite(save_path, undistorted)
+        print(f"[INFO] Saved undistorted image: {save_path}")
+    else:
+        print(f"[ERROR] Could not load image: {img_path}")
 
-        ret, corners = cv2.findChessboardCorners(
-            gray, board_size,
-            cv2.CALIB_CB_ADAPTIVE_THRESH + 
-            cv2.CALIB_CB_FAST_CHECK + 
-            cv2.CALIB_CB_NORMALIZE_IMAGE)
-            
-        if ret:
-            objpoints.append(objp)
-            corners2 = cv2.cornerSubPix(
-                gray, corner, (11, 11), (-1, -1),
-                (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001))
-            imgpoints.append(corners)
-
-            vis_img = cv2.drawChessboardCorners(img, board_size, corners2, ret)
-            cv2.imshow('Corners', vis_img)
-            cv2.waitKey(500)
-        else:
-            print(f"[WARN] Chessboard not found in {fname}")
-
-    ret, camera_matrix, dist_coeffs, rvecs, tvecs = cv2.calibrateCamera(
-        objpoints, imgpoints, gray.shape[::-1], None, None)
-
-    return camera_matrix, dist_coeffs
-
-def save_intrinsics(filename, camera_matrix, dist_coeffs, rvecs, tvecs):
-    data = {
-        'camera_matrix': camera_matrix.tolist(),
-        'dist_coeffs': dist_coeffs.tolist(),
-        'rotation_vectors': [rvec.tolist() for rvec in rvecs],
-        'translation_vectors': [tvec.tolist() for tvec in tvecs]
-    }
-    with open(filename, 'w') as f:
-        json.dump(data, f, indent=2)
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--images', required=True, help='Path to folder containing chessboard images')
-    parser.add_argument('--out', required=True, help='Output JSON file for intrinsics')
-    parser.add_argument('--square', type=float, default=0.0244, help='Chessboard square size in meters')
-    parser.add_argument('--board', default='6x9', help='Chessboard size as colsxrows (e.g. 6x9)')
-
-    args = parser.parse_args()
-    board_size = tuple(map(int, args.board.split('x')))
-
-    print(f"[INFO] Using board size: {board_size}")
-    print(f"[INFO] Using square size: {args.square} meters")
-
-    camera_matrix, dist_coeffs, rvecs, tvecs = calibrate_camera(args.images, board_size, args.square)
-    save_intrinsics(args.out, camera_matrix, dist_coeffs, rvecs, tvecs)
-
-    print(f"[INFO] Calibration complete. Results saved to {args.out}")
-    print(f"[INFO] Camera matrix:\n{camera_matrix}")
-    print(f"[INFO] Distortion coefficients:\n{dist_coeffs}")
+for img_path in glob.glob(os.path.join(input_dir_cam2, '*.jpg')):
+    img = cv2.imread(img_path)
+    if img is not None:
+        undistorted = cv2.undistort(img, camera_matrix2, dist_coeffs2)
+        filename = os.path.basename(img_path)
+        save_path = os.path.join(output_dir_cam2, f'undistorted_{filename}')
+        cv2.imwrite(save_path, undistorted)
+        print(f"[INFO] Saved undistorted image: {save_path}")
+    else:
+        print(f"[ERROR] Could not load image: {img_path}")
