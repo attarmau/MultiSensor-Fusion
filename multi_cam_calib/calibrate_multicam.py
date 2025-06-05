164ebdc74806b7a32f41a6c100fe1f967e83e4b6
attarmau <attarmau111@gmail.com>
2025-06-05 14:36:30 +0800

    Delete multi_cam_calib/calibrate_multicam.py

--- multi_cam_calib/calibrate_multicam.py
+++
@@ -1,9 +0,0 @@
-# Chain camera poses (e.g., Cam0 is origin, Cam1 relative to Cam0, Cam2 relative to Cam1...)
-def compute_global_poses(relative_poses):
-    global_poses = {0: (np.eye(3), np.zeros((3,1)))}
-    for cam_id, (R, T) in relative_poses.items():
-        R_prev, T_prev = global_poses[cam_id - 1]
-        R_global = R @ R_prev
-        T_global = R @ T_prev + T
-        global_poses[cam_id] = (R_global, T_global)
-    return global_poses
